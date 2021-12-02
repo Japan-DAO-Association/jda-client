a97404a3dbed0b89252b41c660d9a891583da1f4
aiinkiestism <g.k.k.r.1228kc@gmail.com>
2021-12-02 15:19:35 +0900

    Update heroarea

--- components/HeroArea.vue
+++ components/HeroArea.vue
@@ -1,32 +1,30 @@
 <template>
   <v-row class="hero">
-    <v-col cols="12">
-      <!-- <h3>Life with DAO</h3> -->
+    <v-col cols="12" class="">
+      <h2 class="hero-text">Life with DAO</h2>
     </v-col>
   </v-row>
 </template>
 
 <style lang="scss" scoped>
 .hero {
-  background-image: url('@/assets/images/heroarea.svg');
-  background-size: 100% auto;
-  height: 60vh;
-  @media (max-width: 1500px) {
-    background-size: auto 100%;
-    background-position: center;
+  background-image: url('@/assets/images/hero_bg.svg');
+  height: 95vh;
+  background-size: auto 100%;
+  background-position: center;
+  position: relative;
+  .hero-text {
+    background: rgba(57, 11, 247, 0.6);
+    padding: 8px 90px;
+    position: absolute;
+    right: 10%;
+    bottom: 34%;
+    font-family: Sanchez;
+    font-style: normal;
+    font-weight: normal;
+    font-size: 72px;
+    line-height: 100px;
+    color: #fff;
   }
-  @media (max-width: 1350px) {
-    height: 55vh;
-  }
-  @media (max-width: 1200px) {
-    height: 48vh;
-  }
-  @media (max-width: 1050px) {
-    height: 42vh;
-  }
-  @media (max-width: 900px) {
-    height: 36vh;
-  }
-  // まだレスポンシブ対応必要だけど一度比率変えた画像をもらう
 }
 </style>

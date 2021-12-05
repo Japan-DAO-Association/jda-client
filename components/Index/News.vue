5359b34b661809749e3a983810dbccf8bfc99ffa
aiinkiestism <g.k.k.r.1228kc@gmail.com>
2021-12-06 02:55:05 +0900

    Add business logic to News

--- components/Index/News.vue
+++ components/Index/News.vue
@@ -1,53 +1,31 @@
 <template>
   <Aos>
     <H3>News</H3>
-    <!-- medium apiから最新の記事の画像、リンク、日付、タイトルをとってくる -->
     <div class="mx-4 mt-8">
-      <v-row>
+      <v-row
+        v-for="article in articles"
+        :key="article.title"
+      >
         <v-col
           cols="3"
         >
-          <a href="#" target="_blank" rel="noopener">
-            <p>Date</p>
+          <a
+            :href="article.link"
+            target="_blank"
+            rel="noopener"
+          >
+            <p>{{ article.pubDate }}</p>
           </a>
         </v-col>
         <v-col
           cols="8"
         >
-          <a href="#" target="_blank" rel="noopener">
-            <p>title of an article</p>
-          </a>
-        </v-col>
-      </v-row>
-      <v-row>
-        <v-col
-          cols="3"
-        >
-          <a href="#" target="_blank" rel="noopener">
-            <p>Date</p>
-          </a>
-        </v-col>
-        <v-col
-          cols="8"
-        >
-          <a href="#" target="_blank" rel="noopener">
-            <p>title of an article</p>
-          </a>
-        </v-col>
-      </v-row>
-      <v-row>
-        <v-col
-          cols="3"
-        >
-          <a href="#" target="_blank" rel="noopener">
-            <p>Date</p>
-          </a>
-        </v-col>
-        <v-col
-          cols="8"
-        >
-          <a href="#" target="_blank" rel="noopener">
-            <p>title of an article</p>
+          <a
+            :href="article.link"
+            target="_blank"
+            rel="noopener"
+          >
+            <p>{{ article.title }}</p>
           </a>
         </v-col>
       </v-row>
@@ -63,6 +41,25 @@ export default {
   components: {
     Aos,
     H3,
+  },
+  data: () => ({
+    articles: [],
+  }),
+  mounted() {
+    this.getArticles();
+  },
+  methods: {
+    async getArticles() {
+      await this.$axios.get(`${process.env.RSS2JSON_URL}https://medium.com/feed/@${process.env.MEDIUM_ACCOUNT_NAME}`).then((res) => {
+        res.data.items.forEach((el) => {
+          this.articles.push(el);
+          // eslint-disable-next-line no-console
+          console.log(el);
+        });
+      }).catch((e) => {
+        throw new Error(e);
+      });
+    },
   }
 }
 </script>

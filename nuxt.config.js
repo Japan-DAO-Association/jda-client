eb47d91b6bc6a7ec7737e3b1278a084b7b63849e
aiinkiestism <g.k.k.r.1228kc@gmail.com>
2021-12-14 16:41:17 +0900

    Add metatag and favicons, etc

--- nuxt.config.js
+++ nuxt.config.js
@@ -18,26 +18,36 @@ export default {// 404 routing
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
     htmlAttrs: {
-      lang: 'ja',
+      lang: 'en',
       prefix: 'og: http://ogp.me/ns#',
     },
-    titleTemplate: '%s - jda-client',
-    title: 'jda-client',
+    titleTemplate: '%s',
+    title: 'Japan DAO Association',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
-      { hid: 'og:site_name', property: 'og:site_name', content: '' },
+      { hid: 'description', name: 'description', content: 'Japan DAO Association is a Pear to Pear Job Matching Community. The purpose of this community is to increase the number of individuals who can make a living from DAOs.' },
+      { hid: 'og:site_name', property: 'og:site_name', content: 'Japan DAO Association' },
       { hid: 'og:type', property: 'og:type', content: 'website' },
       { hid: 'og:url', property: 'og:url', content: '' },
-      { hid: 'og:title', property: 'og:title', content: '' },
-      { hid: 'og:description', property: 'og:description', content: '' },
+      { hid: 'og:title', property: 'og:title', content: 'Japan DAO Association' },
+      { hid: 'og:description', property: 'og:description', content: 'Japan DAO Association is a Pear to Pear Job Matching Community. The purpose of this community is to increase the number of individuals who can make a living from DAOs.' },
       { hid: 'og:image', property: 'og:image', content: '画像のURL' },
       { name: 'twitter:card', content: 'summary' },
-      { name: 'format-detection', content: 'telephone=no' }
+      { name: 'fb:app_id', content: '' },
+      { name: 'format-detection', content: 'telephone=no' },
+      { name: 'msapplication-TileColor', content: '#da532c' },
+      { name: 'theme-color', content: '#ffffff' }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      { rel: 'apple-touch-icon', sizes: '180x180', href: '/apple-touch-icon.png' },
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      { rel: 'icon', type: 'image/png', sizes: '512x512', href: '/android-chrome-512x512.png' },
+      { rel: 'icon', type: 'image/png', sizes: '192x192', href: '/android-chrome-192x192.png' },
+      { rel: 'icon', type: 'image/png', sizes: '32x32', href: '/favicon-32x32.png' },
+      { rel: 'icon', type: 'image/png', sizes: '16x16', href: '/favicon-16x16.png' },
+      { rel: 'manifest', href: 'site.webmanifest' },
+      { rel: 'mask-icon', href: '/safari-pinned-tab.svg', color: '#5bbad5' }
     ],
     script: [
       {

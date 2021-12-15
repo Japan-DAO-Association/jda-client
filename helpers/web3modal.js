76ac35a1e1496aec1442dfd271cb2c6b17e72ab3
aiinkiestism <g.k.k.r.1228kc@gmail.com>
2021-12-15 22:22:15 +0900

    Refactor web3modal.js

--- helpers/web3modal.js
+++ helpers/web3modal.js
@@ -1,35 +1,26 @@
-// import ethers from 'ethers';
 import { ethers } from 'ethers';
 import Web3Modal from 'web3modal';
 import providerOptions from './providerOptions';
 
-export const getWeb3Modal = () => {
-  if (process.browser) {
-    const web3Modal = new Web3Modal({
-      network: "mainnet",
-      cacheProvider: true,
-      providerOptions,
-    })
+const _getWeb3Modal = () => {
+  const web3Modal = new Web3Modal({
+    network: "mainnet",
+    cacheProvider: true,
+    providerOptions,
+  })
 
-    return web3Modal;
-  } else {
-    return null;
-  }
+  return web3Modal;
 }
 
 async function callWeb3Modal() {
-  if (process.browser) {
-    const web3Modal = getWeb3Modal();
-    const web3ModalProvider = await web3Modal.connect();
-    const provider = new ethers.providers.Web3Provider(web3ModalProvider);
-    const accounts = await provider.listAccounts();
+  const web3Modal = _getWeb3Modal();
+  const web3ModalProvider = await web3Modal.connect();
+  const provider = new ethers.providers.Web3Provider(web3ModalProvider);
+  const accounts = await provider.listAccounts();
 
-    return {
-      account: accounts[0],
-      provider,
-    }
-  } else {
-    return null;
+  return {
+    account: accounts[0],
+    provider,
   }
 }
 

bdf9154f4b08f1e56d0bbe72ac719b43dd8f2bf0
aiinkiestism <g.k.k.r.1228kc@gmail.com>
2021-12-20 19:53:53 +0900

    Fix heroarea

--- components/Index/HeroArea.vue
+++ components/Index/HeroArea.vue
@@ -12,7 +12,7 @@
           ></Textra>
           with DAO
         </h2>
-        <H3 class="hero-text">Realizing the Next Generation of Work with DAO<br />To live our oun life</H3>
+        <H3 class="hero-text hero-text-h3">Realizing the Next Generation of Work with DAO<br />To live our oun life</H3>
       </div>
       <DiscordHeroArea class="discord-heroarea" />
     </v-col>
@@ -47,10 +47,13 @@ export default {
 <style lang="scss" scoped>
 .hero {
   background-image: url('@/assets/images/hero_bg3.svg');
-  height: 95vh;
+  height: 100vh;
   background-size: auto 100%;
   background-position: center;
   position: relative;
+  @media (max-width: 440px) {
+    height: 95vh;
+  }
   .hero-text-wrap {
     position: absolute;
     top: 50%;
@@ -58,6 +61,12 @@ export default {
     transform: translate(-50%, -50%);
     -webkit-transform: translate(-50%, -50%);
     -ms-transform: translate(-50%, -50%);
+    width: 80%;
+    max-width: 80%;
+    @media (max-width: 580px) {
+      width: 90%;
+      max-width: 90%;
+    }
     .hero-text {
       font-family: Sanchez !important;
       font-style: normal;
@@ -71,13 +80,23 @@ export default {
       @media (max-width: 580px) {
         right: 0;
         padding: 8px 24px;
-        font-size: 40px;
-        line-height: 64px;
       }
     }
     .hero-text-h2 {
       font-size: 72px;
       line-height: 100px;
+      @media (max-width: 580px) {
+        font-size: 50px;
+        line-height: 34px;
+      }
+      @media (max-width: 440px) {
+        font-size: 38px;
+      }
+    }
+    .hero-text-h3 {
+      @media(max-width: 580px) {
+        font-size: 26px !important;
+      }
     }
   }
   .discord-heroarea {
@@ -85,6 +104,16 @@ export default {
     bottom: 10%;
     width: 100%;
     padding: 0 28%;
+    @media (max-width: 1400px) {
+      padding: 0 20%;
+    }
+    @media (max-width: 1100px) {
+      bottom: 8%;
+      padding: 0 16%;
+    }
+    @media (max-width: 580px) {
+      display: none;
+    }
   }
 }
 </style>

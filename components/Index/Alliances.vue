16c000d90bc2baa3fb846415fe4479671d37ecc1
aiinkiestism <g.k.k.r.1228kc@gmail.com>
2021-12-22 16:26:38 +0900

    Add affiliates communities

--- components/Index/Alliances.vue
+++ components/Index/Alliances.vue
@@ -3,27 +3,18 @@
     <v-row>
       <v-col>
         <H3>Affiliated DAO Communities</H3>
-        <!-- <H3>提携DAOコミュニティ</H3> -->
         <div class="mx-4 mt-8">
-          <v-row class="alliance-row">
-            <v-col>
+          <v-row
+            class="alliance-row"
+          >
+            <v-col
+              v-for="item in items"
+              :key="item.name"
+              cols="12" xs="12" sm="6" md="4" lg="4"
+            >
               <div class="card-border">
                 <v-card class="card">
-                  <v-card-title class="card-title">ブロックチェーン研究所</v-card-title>
-                </v-card>
-              </div>
-            </v-col>
-            <v-col>
-              <div class="card-border">
-                <v-card class="card">
-                  <v-card-title class="card-title">Soundan NFT</v-card-title>
-                </v-card>
-              </div>
-            </v-col>
-            <v-col>
-              <div class="card-border">
-                <v-card class="card">
-                  <v-card-title class="card-title">DfinityJP</v-card-title>
+                  <v-card-title class="card-title">{{ item.name }}</v-card-title>
                 </v-card>
               </div>
             </v-col>
@@ -42,15 +33,33 @@ export default {
   components: {
     Aos,
     H3,
-  }
+  },
+  data: () => ({
+    items: [
+      { name: 'ブロックチェーン研究所' },
+      { name: 'Soundan NFT' },
+      { name: 'Dfinity JP' },
+      { name: 'KudasaiJP' },
+      { name: 'NFTJPN' },
+      { name: 'ICP LAB JP' },
+      { name: 'Ninja DAO' },
+      { name: 'ソフィクラ' },
+      { name: 'HODLer会' },
+      { name: 'TITAN DAO' },
+      { name: 'クリプト金玉' },
+      { name: 'Yield Farming Lab（YFL）' },
+      { name: 'Airdrop JAPAN' },
+      { name: 'and more soon..' },
+    ]
+  })
 }
 </script>
 
 <style lang="scss" scoped>
 .alliance-row {
-  @media (max-width: 600px) {
-    display: block;
-  }
+  // @media (max-width: 600px) {
+  //   display: block;
+  // }
   .card-border {
     background: linear-gradient(to right, #467de7, #da46e7);
     padding: 2px;
